ec3d1bf570ef655ebc1d39f4603232c0fb7cc630
wayneleon1 <rurangwaleon10@gmail.com>
2025-03-08 15:58:17 +0200

    Implementing Authentication

--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -1,10 +1,68 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { useRouter } from "expo-router";
+import { View, Text, TouchableOpacity, TextInput } from "react-native";
+import { supabase } from "@/utils/supabase";
 
-export default function Signup() {
+export default function () {
+  const router = useRouter();
+  const [email, setEmail] = React.useState<string>("");
+  const [password, setPassword] = React.useState<string>("");
+  const [username, setUsername] = React.useState<string>("");
+
+  const handleSignup = async () => {
+    // supabase Auth
+    const { data, error } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+    });
+    if (error) return console.error(error);
+
+    // Insert results into Db
+    const { error: userError } = await supabase.from("User").insert({
+      id: data.user?.id,
+      username: username,
+      email: email,
+    });
+    if (error) return console.error(userError);
+
+    router.back();
+    router.push("/(tabs)");
+  };
+
   return (
-    <View>
-      <Text>signup</Text>
+    <View className="flex-1 justify-center bg-white items-center">
+      <View className="w-full p-4">
+        <Text className="text-black font-bold text-3xl text-center mb-4">
+          Signup
+        </Text>
+        <TextInput
+          placeholder="Username"
+          value={username}
+          onChangeText={setUsername}
+          className="bg-white p-4 rounded-lg border border-gray-300 w-full mb-4"
+        />
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+          className="bg-white p-4 rounded-lg border border-gray-300 w-full mb-4"
+        />
+        <TextInput
+          secureTextEntry={true}
+          placeholder="password"
+          value={password}
+          onChangeText={setPassword}
+          className="bg-white p-4 rounded-lg border border-gray-300 mb-4 w-full"
+        />
+        <TouchableOpacity
+          className="rounded-lg px-4 py-2  bg-black "
+          onPress={handleSignup}
+        >
+          <Text className="text-white font-bold text-lg text-center">
+            Signup
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
 import { Tabs } from "expo-router";
 import React from "react";
-import Ionicons from '@expo/vector-icons/Ionicons';
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { View } from "react-native";
 
 export default function TabLayout() {
@@ -15,39 +15,63 @@ export default function TabLayout() {
         name="index"
         options={{
           title: "Home",
-          tabBarIcon: ({focused})=><Ionicons name={`${focused ? 'home-sharp' : 'home-outline'}`} size={24} color="black" />
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={`${focused ? "home-sharp" : "home-outline"}`}
+              size={24}
+              color="black"
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="friends"
         options={{
           title: "Friends",
-          tabBarIcon: ({focused})=><Ionicons name={`${focused ? 'people' : 'people-outline'}`} size={24} color="black" />
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={`${focused ? "people" : "people-outline"}`}
+              size={24}
+              color="black"
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="camera"
         options={{
           title: "",
-          tabBarIcon: ()=>
+          tabBarIcon: () => (
             <View className="absolute justify-center items-center -bottom-4 h-[75px] w-[75px]">
-              <Ionicons name='add-circle' size={75} color="black" />
-          </View>
+              <Ionicons name="add-circle" size={75} color="black" />
+            </View>
+          ),
         }}
       />
       <Tabs.Screen
         name="inbox"
         options={{
           title: "Inbox",
-          tabBarIcon: ({focused})=>
-            <Ionicons name={`${focused ? 'chatbox-ellipses' : 'chatbox-ellipses-outline'}`} size={24} color="black" />
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={`${focused ? "chatbox-ellipses" : "chatbox-ellipses-outline"}`}
+              size={24}
+              color="black"
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: "Profile",
-          tabBarIcon: ({focused})=><Ionicons name={`${focused ? 'person' : 'person-outline'}`} size={24} color="black" />
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={`${focused ? "person" : "person-outline"}`}
+              size={24}
+              color="black"
+            />
+          ),
         }}
       />
     </Tabs>
